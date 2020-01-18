227d599625183a53feecc0cbbd6787039d83e900
Nuthi Sriram <46497115+Nuthi-Sriram@users.noreply.github.com>
2020-01-18 17:28:49 +0530

    adding simple
    Signed-off-by: Nuthi Sriram <46497115+Nuthi-Sriram@users.noreply.github.com>

---
+++ SourceCode/addition.asm
@@ -0,0 +1,11 @@
+.MODEL small            ;mentioning the size of the code
+.STACK
+.DATA                   ;WHERE ALL THE VARIABLES ARE DECLARED
+.CODE                    ;From here in all the data will  go here  
+mov ax,100
+mov bx,6
+add ax,bx
+                                                                   
+                                                                   
+end                                                                   
+                                                                                                                
